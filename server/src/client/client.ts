e8ace4e2fdadafcf4c3146640ed3499057056329
shady-2004 <shadyshiko2004@gmail.com>
2025-10-07 20:54:18 +0300

    feat(client):terminate app if cannot connect to the db

--- server/src/client/client.ts
+++ server/src/client/client.ts
@@ -1,35 +1,32 @@
 import mongoose from "mongoose";
 
 async function connect() {
-  try {
-    if (!process.env.DATABASE || !process.env.DATABASE_PASSWORD) {
-      throw new Error("Missing DATABASE or DATABASE_PASSWORD in .env");
-    }
-
-    let uri = process.env.DATABASE;
-
-    const clientOptions = {
-      serverApi: {
-        version: "1" as const,
-        strict: true,
-        deprecationErrors: true,
-      },
-    };
-
-    await mongoose.connect(uri, clientOptions);
-
-    if (mongoose.connection.db) {
-      await mongoose.connection.db.admin().command({ ping: 1 as const });
-    } else {
-      throw new Error("Database connection is not established.");
-    }
-
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } catch (err) {
-    console.error("DB connection error:", err);
+  if (!process.env.DATABASE || !process.env.DATABASE_PASSWORD) {
+    throw new Error("Missing DATABASE or DATABASE_PASSWORD in .env");
   }
+
+  const uri = process.env.DATABASE.replace(
+    "<password>",
+    process.env.DATABASE_PASSWORD
+  );
+
+  const clientOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 10000,
+    serverApi: {
+      version: "1" as const,
+      strict: true,
+      deprecationErrors: true,
+    },
+  };
+
+  await mongoose.connect(uri, clientOptions);
+
+  mongoose.connection.once("open", async () => {
+    await mongoose.connection.db.admin().ping();
+    console.log("Successfully connected and pinged MongoDB!");
+  });
 }
 
 export default connect;
