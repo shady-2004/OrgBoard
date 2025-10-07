12ce047e9018de26a71ce6c53b4e3d0bc76d58c4
shady-2004 <shadyshiko2004@gmail.com>
2025-10-07 19:54:58 +0300

    fix

--- server/src/routes/authRouter.ts
+++ server/src/routes/authRouter.ts
@@ -1,11 +1,12 @@
 import{ Router } from "express";
 import authController from "../controllers/auth.controller";
 import protect from "../middlewares/protect";
+import restrict from "../middlewares/restrict";
 
 const authRouter = Router();
 
 authRouter.post("/login", authController.login);
-authRouter.post("/signup", authController.signUp);
+authRouter.post("/signup",restrict('shady'), authController.signUp);
 authRouter.patch("/update-password", protect, authController.changePassword);
 authRouter.get("/me", protect, authController.me);
 
